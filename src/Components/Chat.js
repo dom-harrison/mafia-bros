188f9b287f0fcf82d3d8450428fb37c3aea6ec10
Dominic Harrison <a9293480@9293480s-MacBook-Pro.local>
2020-05-06 22:21:42 +0100

    More style changes

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -15,16 +15,16 @@ const Chat = ({ roomStatus = {}, messages = [], submitNewMessage, handleLeaveRoo
     }
 
     const conversation = messages.map(message => (
-      <li>{message}</li>
+      <li className="message">{message}</li>
     ))
 
     return (
       <div className="chat section">
         <div>Room: {roomStatus.name}</div>
         <div>Users: {(roomStatus.users || []).map(user => `${user}, `)}</div>
-        <ul>{conversation}</ul>
-        <form onSubmit={handleSubmit}>
-          <input value={newMessage} onChange={handleChange} /><button type='submit'>Send</button>
+        <ul className="conversation">{conversation}</ul>
+        <form className="message-input" onSubmit={handleSubmit}>
+          <input value={newMessage} onChange={handleChange} /><button className="send button" type='submit'>Send</button>
         </form>
         <button className="room-button" onClick={handleLeaveRoom}>Leave room</button>
       </div>
