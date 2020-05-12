444e5a157ba29c699e7ff58e64ce997188b752e6
Dom Harrison <dom_harrison@live.co.uk>
2020-05-12 18:04:55 +0100

    Docker updates, API updates

---
+++ src/api/index.js
@@ -0,0 +1,42 @@
+import socketIOClient from "socket.io-client";
+
+const hostName = window.location.hostname;
+const endpoint = `${hostName}:4000`;
+// const endpoint = 'mb-server:4000'
+const socket = socketIOClient(endpoint);
+
+socket.on('error', error => {
+  console.log(error)
+})
+socket.on('connect', res => {
+  console.log('connect')
+})
+socket.on('connect_error', (error) => {
+  console.log(error)
+})
+
+
+export const newMessage = (cb) => {
+  // listen for any messages coming through
+  // of type 'new_message' and then trigger the
+  // callback function with said message
+  socket.on("new_message", message => {
+    // console.log the message for posterity
+    console.log(message);
+    // trigger the callback passed in when
+    // our App component calls connect
+    cb(message);
+  });
+}
+
+export const roomStatus = (cb) => {
+    socket.on("room_status", message => {
+      console.log(message);
+      cb(message);
+    });
+  }
+
+export const emit = (type, message) => {
+    // emit a message of type 'type
+    socket.emit(type, message);
+}
