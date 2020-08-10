994a09c09c0163a54d9982ab117f4e2f931fe7ae
Dom Harrison <dom_harrison@live.co.uk>
2020-08-10 13:35:58 +0100

    Flip card css

--- src/Components/UserTile.js
+++ src/Components/UserTile.js
@@ -15,12 +15,17 @@ const UserTile = ({ user = {}, index = 0, selected = false, handleUserClick, you
     return (
       <div className={`user${selected ? ' selected' : ''}${you ? ' you' : ''}${revoteCandidate ? ' revote' : ''}${dead ? ' dead' : ''}`}  onClick={() => dead ? false : handleUserClick(name, index)}>
         <div className={`user-text${dead ? ' dead' : ''}`}>{name}</div>
-        <div className={`card ${role ? role : 'unknown'}${dead ? ' dead' : ''}`}>
-          <span className="top">{symbol}</span>
-          <span>{role || '?'}</span>
-          <span className="bottom">{symbol}</span>
-          <div className={`user-votes ${!userVotes ? ' hidden' : ''}`}>{userVotes}</div>
+        <div className={`card ${role ? 'known' : 'unknown'}${dead ? ' dead' : ''}`}>
+          <div className='front'>
+            <span>{'?'}</span>
+          </div>
+          <div className={`back ${role}`}>
+            <span className="top">{symbol}</span>
+            <span>{role}</span>
+            <span className="bottom">{symbol}</span>
+          </div>
         </div>
+        <div className={`user-votes ${!userVotes ? ' hidden' : ''}`}>{userVotes}</div>
       </div>
     );
   };
